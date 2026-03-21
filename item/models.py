0cbeeab561da902a6090751f1d3af32b9ed2961b
Urmanas <Urmanas.19@gmail.com>
2026-03-21 22:33:22 +0600

    Updated models

--- item/models.py
+++ item/models.py
@@ -3,4 +3,11 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name 
+
+class Item(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
     
